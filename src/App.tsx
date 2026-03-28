c356e7a7318b2809916fae8614b471518b31834a
Wolfrand <wolfrand90@gmail.com>
2026-03-27 21:46:21 -0300

    created dark mobile page design; imported new components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import TodoForm from "./Components/TodoForm";
+import TodoHeader from "./Components/TodoHeader";
+import TodoList from "./Components/TodoList";
+
 function App() {
-
   return (
-    <>
-    </>
-  )
-}
+    <main className="bg-very-dark-blue h-screen">
+      <div className="bg-[url(/images/bg-desktop-dark.jpg)] h-80 bg-cover bg-center">
+        <div className="max-w-175 m-auto p-8">
+          <TodoHeader></TodoHeader>
 
-export default App
+          <TodoForm></TodoForm>
+
+          <TodoList></TodoList>
+        </div>
+      </div>
+    </main>
+  )
+};
+
+export default App;
