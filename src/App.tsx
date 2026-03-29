8f442cb1fa40770ba08c3cd445e87315ec06e2ec
Wolfrand <wolfrand90@gmail.com>
2026-03-29 14:06:36 -0300

    feat: add base layer background images for dark and light themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import TodoForm from "./Components/TodoForm";
-import TodoHeader from "./Components/TodoHeader";
-import TodoList from "./Components/TodoList";
+import TodoForm from "./components/TodoForm";
+import TodoHeader from "./components/TodoHeader";
+import TodoList from "./components/TodoList";
+import { themeConfig } from "./contexts/theme";
+import { ThemeContext } from "./contexts/ThemeContext";
+import { useContext } from "react";
+
 
 function App() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <main className="bg-very-dark-blue h-screen">
-      <div className="bg-[url(/images/bg-desktop-dark.jpg)] h-80 bg-cover bg-center">
+    <main className={`${themeConfig[theme].layout.backgroundColor} h-screen`}>
+      <div className={`${themeConfig[theme].layout.heroClass}`}>
         <div className="max-w-175 m-auto p-8">
           <TodoHeader></TodoHeader>
 
