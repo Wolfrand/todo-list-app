a32429f27f487ed09802c1b1d406e688aa8fa267
Wolfrand <wolfrand90@gmail.com>
2026-03-29 21:53:00 -0300

    feat: add mobile filter container div

--- src/Components/TodoList/index.tsx
+++ src/Components/TodoList/index.tsx
@@ -3,7 +3,9 @@ import { ThemeContext } from "../../contexts/ThemeContext";
 import { useContext } from "react";
 
 const TodoList = () => {
-    const { theme } = useContext(ThemeContext);
+    const context = useContext(ThemeContext);
+
+    const { theme } = context;
 
     const todos = [
         { id: 1, text: "Todo 1" },
@@ -12,29 +14,39 @@ const TodoList = () => {
     ];
 
     return (
-        <div className={`${themeConfig[theme].todo.backgroundColor} rounded-md`}>
-            <ul>
-                {todos.map((todo) => (
-                    <li key={todo.id} className={`flex items-center ${themeConfig[theme].todo.textColor} p-6 text-lg border-b ${themeConfig[theme].todo.borderColor} gap-4`}>
-                        <button className={`w-6 h-6 border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer`}></button>
+        <>
+            <div className={`${themeConfig[theme].todo.backgroundColor} rounded-md`}>
+                <ul>
+                    {todos.map((todo) => (
+                        <li key={todo.id} className={`flex items-center ${themeConfig[theme].todo.textColor} p-5 sm:text-lg border-b ${themeConfig[theme].todo.borderColor} gap-4`}>
+                            <span className="w-6 h-6 rounded-full hover:bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))] hover:p-px">
+                                <button className={`w-full h-full border ${themeConfig[theme].todo.borderColor} ${themeConfig[theme].todo.backgroundColor} rounded-full cursor-pointer`}></button>
+                            </span>
 
-                        <p>{todo.text}</p>
-                    </li>
-                ))}
-            </ul>
+                            <p className="pt-1">{todo.text}</p>
+                        </li>
+                    ))}
+                </ul>
 
-            <div className={`flex justify-between ${themeConfig[theme].filter.textColor} text-sm px-6 py-4`}>
-                <p>{todos.length} items left</p>
+                <div className={`flex justify-between ${themeConfig[theme].filter.textColor} text-sm px-6 py-4`}>
+                    <p>{todos.length} items left</p>
 
-                <div className="flex gap-4">
-                    <button className="cursor-pointer">All</button>
-                    <button className="cursor-pointer">Active</button>
-                    <button className="cursor-pointer">Completed</button>
+                    <div className="hidden sm:flex gap-4">
+                        <button className={`cursor-pointer font-bold ${themeConfig[theme].filter.hover} text-bright-blue`}>All</button>
+                        <button className={`cursor-pointer font-bold ${themeConfig[theme].filter.hover}`}>Active</button>
+                        <button className={`cursor-pointer font-bold ${themeConfig[theme].filter.hover}`}>Completed</button>
+                    </div>
+
+                    <button className={`cursor-pointer ${themeConfig[theme].filter.hover}`}>Clear completed</button>
                 </div>
-
-                <button className="cursor-pointer">Clear completed</button>
             </div>
-        </div>
+
+            <div className={` flex justify-center gap-5 py-4 rounded-md mt-4 ${themeConfig[theme].todo.backgroundColor} ${themeConfig[theme].filter.textColor} sm:hidden`}>
+                <button className={`cursor-pointer font-bold ${themeConfig[theme].filter.hover} text-bright-blue`}>All</button>
+                <button className={`cursor-pointer font-bold ${themeConfig[theme].filter.hover}`}>Active</button>
+                <button className={`cursor-pointer font-bold ${themeConfig[theme].filter.hover}`}>Completed</button>
+            </div>
+        </>
     )
 };
 
